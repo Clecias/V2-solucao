69b95b6e4105a177b5601837409e70abc9a69f99
Routine40 <cleciabi@gmail.com>
2026-02-03 23:12:14 +0000

    Fix promo popup initialization

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { initTracking } from './utils/tracking';
+import { initPromoPopup } from './utils/promoPopup';
 initTracking();
+initPromoPopup();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
